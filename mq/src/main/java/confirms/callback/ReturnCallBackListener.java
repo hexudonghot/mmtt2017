6ee603c91b4479edd633d0fe27c4c3bfe83e6c58
hexudong <123456>
2017-09-14 10:05:57 +0800

    init

---
+++ mq/src/main/java/confirms/callback/ReturnCallBackListener.java
@@ -0,0 +1,20 @@
+package confirms.callback;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate.ReturnCallback;
+import org.springframework.stereotype.Service;
+
+@Service("returnCallBackListener")
+public class ReturnCallBackListener implements ReturnCallback {
+
+	public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+		String msgId = "";
+		if (message.getMessageProperties().getCorrelationId() != null) {
+			msgId = new String(message.getMessageProperties().getCorrelationId());
+		}
+		System.out.println("return--message: msgId:" + msgId + ",msgBody:" + new String(message.getBody())
+				+ ",replyCode:" + replyCode + ",replyText:" + replyText + ",exchange:" + exchange + ",routingKey:"
+				+ routingKey);
+	}
+
+}
